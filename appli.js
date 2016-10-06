e332814b9e03695414dd12c6dc67f7165d425baf
nikia <nsovet@laposte.net>
2016-10-06 15:57:41 +0200

    creation objet perso

---
+++ appli.js
@@ -0,0 +1,61 @@
+// gentil//
+
+var gentil = {
+ name:"judy";
+ age:"20"
+ race:"rabbit";
+ job:"police officer";
+ local:"48.440028, 1.482639"
+
+},
+
+
+{
+ name:"Nick";
+ age:"con artist"
+ race:"fox";
+ job:"22";
+ local:"48.440028, 1.482639"
+
+};
+
+//ennemis//
+
+var mechant={
+ name:"koslov";
+ age:"30"
+ race:"polar bear";
+ job:"bodyguard";
+ local:""
+
+},
+
+
+{
+ name:"bellwether";
+ age:"28"
+ race:"sheep";
+ job:"assistant mayor";
+ local:""
+
+};
+
+
+
+{
+ name:"Duke";
+ age:"20"
+ race:"weasel";
+ job:"robber";
+ local:""
+
+};
+
+{
+ name:"Doug";
+ age:"30"
+ race:"ship";
+ job:"chimist";
+ local:""
+
+};
